56080f6bba24802d88d34679135c2916748de623
Number Empire Dev <guangyi061231@users.noreply.github.com>
2026-06-21 16:08:44 +0800

    chore: update Bundle ID to com.MrGaryW.petmate

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.petmate.app',
+  appId: 'com.MrGaryW.petmate',
   appName: 'PetMate',
   webDir: 'out',
   server: {
